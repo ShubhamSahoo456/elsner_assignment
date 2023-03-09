5d21f7e13e8dbf513b2dfefcea21188c8f84f6de
shubhamsahookiwi <shubham.sahoo@kiwitech.com>
2023-03-10 02:48:28 +0530

    commit 2

--- backend/index.js
+++ backend/index.js
@@ -15,14 +15,14 @@ app.use("/api/v1", gameRouter);
 const server = http.createServer(app);
 let users = [];
 
-const addNewuser = (gameId, socketId) => {
-  !users.some((user) => user.gameId === gameId) &&
-    users.push({ gameId, socketId });
-};
+// const addNewuser = (gameId, socketId) => {
+//   !users.some((user) => user.gameId === gameId) &&
+//     users.push({ gameId, socketId });
+// };
 
-const getUser = (receiverId) => {
-  return users.find((ele) => ele.userId === receiverId);
-};
+// const getUser = (receiverId) => {
+//   return users.find((ele) => ele.userId === receiverId);
+// };
 
 const io = socket(server, {
   cors: {
@@ -33,27 +33,42 @@ const io = socket(server, {
 
 io.on("connection", (socket) => {
   console.log("a user is connected to backend socket server");
-  socket.on("sendHistory", ({ userId }) => {
-    addNewuser(userId, socket.id);
+  users.push(socket.id);
 
-    console.log(users);
-    io.emit("getAllUsers", users);
+  io.to(socket.id).emit("players", {
+    currentUser: socket.id,
+    users,
+  });
 
-    socket.on("disconnect", () => {
-      console.log("a user is disconnected");
-      // removeUser(socket.id);
-    });
-
-    socket.on("history", ({ currentMove, gamehistory, roomid }) => {
-      const user = getUser(receiverId);
-      console.log(text);
-      io.to(user.socketId).emit("getmessages", {
-        currentMove,
-        history: gamehistory,
-        roomid,
-      });
+  socket.on("sendHistory", ({ senderId, currentMove, gamehistory, roomid }) => {
+    const receiverId = users.filter((ele) => ele !== senderId);
+    io.to(receiverId[0]).emit("history", {
+      currentMove,
+      history: gamehistory,
+      roomid,
     });
   });
+  // socket.on("sendHistory", ({ userId }) => {
+  //   addNewuser(userId, socket.id);
+
+  //   console.log(users);
+  //   io.emit("getAllUsers", users);
+
+  //   socket.on("disconnect", () => {
+  //     console.log("a user is disconnected");
+  //     // removeUser(socket.id);
+  //   });
+
+  //   socket.on("history", ({ currentMove, gamehistory, roomid }) => {
+  //     const user = getUser(receiverId);
+  //     console.log(text);
+  //     io.to(user.socketId).emit("getmessages", {
+  //       currentMove,
+  //       history: gamehistory,
+  //       roomid,
+  //     });
+  //   });
+  // });
 });
 
 server.listen(8000, () => {
